da04fe487808f31489f05d8c70e31f886ccb3855
DeliaAyuNandhitaPratama21 <156631479+DeliaAyuNandhitaPratama21@users.noreply.github.com>
2025-03-23 17:52:23 +0700

    Add files via upload

---
+++ Aplikasi Cuaca/src/thread/ResponCuaca.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thread;
+import java.util.List;
+/**
+ *
+ * @author ASUS
+ */
+public class ResponCuaca {
+    private String name;
+    private Main main;
+    private Wind wind;
+    private List<Weather> weather;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public List<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(List<Weather> weather) {
+        this.weather = weather;
+    }
+
+    public static class Main {
+        private double temp;
+        private int humidity;
+
+        public double getTemp() {
+            return temp;
+        }
+
+        public void setTemp(double temp) {
+            this.temp = temp;
+        }
+
+        public int getHumidity() {
+            return humidity;
+        }
+
+        public void setHumidity(int humidity) {
+            this.humidity = humidity;
+        }
+    }
+
+    public static class Wind {
+        private double speed;
+
+        public double getSpeed() {
+            return speed;
+        }
+
+        public void setSpeed(double speed) {
+            this.speed = speed;
+        }
+    }
+
+    public static class Weather {
+        private String main;
+       
+
+        public String getMain() {
+            return main;
+        }
+
+        public void setMain(String main) {
+            this.main = main;
+        }
+    }
+}
